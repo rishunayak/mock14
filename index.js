d74b1a4526339291b18da89729eeb69266d0923d
Rishu Kumar Nayak <57229844+rishunayak@users.noreply.github.com>
2023-03-04 14:46:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+require("dotenv").config()
+
+const express=require("express")
+const cors=require("cors")
+const connect=require("./Config/Config")
+const user=require("./Routes/user.route")
+//routes
+
+
+const app=express()
+
+app.use(express.json())
+app.use(cors())
+
+app.use("/users",user)
+//route use
+
+
+
+
+
+
+app.get("/",(req,res)=>
+{
+    res.send("Welecome to server")
+})
+
+
+app.listen(process.env.PORT,async()=>
+{
+    await connect
+    console.log("Server started")
+})
